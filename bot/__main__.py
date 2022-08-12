d9a4e56fc3fa34398615ce947295e303c41802c6
YourGirFriendsBF <109566119+YourGirFriendsBF@users.noreply.github.com>
2022-08-12 19:38:47 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,22 +44,15 @@ def stats(update, context):
     mem_t = get_readable_file_size(memory.total)
     mem_a = get_readable_file_size(memory.available)
     mem_u = get_readable_file_size(memory.used)
-    stats = f'<b>Commit Date:</b> {last_commit}\n\n'\
-            f'<b>Bot Uptime:</b> {currentTime}\n'\
-            f'<b>OS Uptime:</b> {osUptime}\n\n'\
-            f'<b>Total Disk Space:</b> {total}\n'\
-            f'<b>Used:</b> {used} | <b>Free:</b> {free}\n\n'\
-            f'<b>Upload:</b> {sent}\n'\
-            f'<b>Download:</b> {recv}\n\n'\
-            f'<b>CPU:</b> {cpuUsage}%\n'\
-            f'<b>RAM:</b> {mem_p}%\n'\
-            f'<b>DISK:</b> {disk}%\n\n'\
-            f'<b>Physical Cores:</b> {p_core}\n'\
-            f'<b>Total Cores:</b> {t_core}\n\n'\
-            f'<b>SWAP:</b> {swap_t} | <b>Used:</b> {swap_p}%\n'\
-            f'<b>Memory Total:</b> {mem_t}\n'\
-            f'<b>Memory Free:</b> {mem_a}\n'\
-            f'<b>Memory Used:</b> {mem_u}\n'
+    stats = f'<b><i><u>{TITLE_NAME} Bot Statistics</u></i></b>\n\n'\
+            f'<b>CPU</b>:  {progress_bar(cpuUsage)} {cpuUsage}%\n' \
+            f'<b>RAM</b>: {progress_bar(mem_p)} {mem_p}%\n' \
+            f'<b>DISK</b>: {progress_bar(disk)} {disk}%\n\n' \
+            f'<b>Updated:</b> {last_commit}\n'\
+            f'<b>I am Working For:</b> <code>{currentTime}</code>\n\n'\
+            f'<b>Total Disk:</b> <code>{total}</code> [{disk}% In use]\n'\
+            f'<b>Used:</b> <code>{used}</code> | <b>Free:</b> <code>{free}</code>\n'\
+            f'<b>T-UL:</b> <code>{sent}</code> | <b>T-DL:</b> <code>{recv}</code>\n'
     sendMessage(stats, context.bot, update.message)
 
 
@@ -75,7 +68,7 @@ Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
     else:
-        sendMarkup('Not Authorized user, deploy your own mirror-leech bot', context.bot, update.message, reply_markup)
+        sendMarkup('Not Authorized user, Join @DhruvMirrorUpdates if you wanna use me!', context.bot, update.message, reply_markup)
 
 def restart(update, context):
     restart_message = sendMessage("Restarting...", context.bot, update.message)
